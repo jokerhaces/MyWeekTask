caea1385a6464ef280498b601b5426834155f617
Jouko Romppainen <106674714+jokerhaces@users.noreply.github.com>
2022-07-22 09:13:33 +0300

    Add files via upload

---
+++ src/main/java/com/example/myseventhtask2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myseventhtask2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txt;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt=(TextView) findViewById(R.id.myText);
+        btn=(Button) findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt.setText("Hello World!");
+            }
+        });
+    }
+}
